49e29531ffa35cc76c718f81c3cc885f09378f35
EhabShaaban <ehabsfeki@gmail.com>
2022-01-28 07:50:57 +0100

    Add py linting

--- stop_instances.py
+++ stop_instances.py
@@ -3,19 +3,21 @@ import boto3
 # Boto Connection
 ec2 = boto3.resource('ec2', 'us-west-2')
 
+
 def lambda_handler(event, context):
-  # Filters
-  filters = [{
-      'Name': 'tag:Name',
-      'Values': ['AutoStop']
+    # Filters
+    filters = [{
+        'Name': 'tag:Name',
+        'Values': ['AutoStop']
     }
-  ]
+    ]
 
-  # Filter running instances that should stop
-  instances = ec2.instances.filter(Filters=filters)
+    # Filter running instances that should stop
+    instances = ec2.instances.filter(Filters=filters)
 
-  # Retrieve instance IDs
-  instance_ids = [instance.id for instance in instances]
+    # Retrieve instance IDs
+    instance_ids = [instance.id for instance in instances]
 
-  # stopping instances
-  stopping_instances = ec2.instances.filter(Filters=[{'Name': 'instance-id', 'Values': instance_ids}]).stop()
+    # stopping instances
+    stopping_instances = ec2.instances.filter(
+        Filters=[{'Name': 'instance-id', 'Values': instance_ids}]).stop()
