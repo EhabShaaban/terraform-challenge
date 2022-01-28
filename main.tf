128772fc9bab50c0f97c706117d3045f10fdc087
EhabShaaban <ehabsfeki@gmail.com>
2022-01-28 07:51:28 +0100

    Add stop apigw

--- main.tf
+++ main.tf
@@ -63,16 +63,16 @@ resource "aws_s3_bucket" "bucket" {
 
 resource "aws_s3_bucket_object" "object" {
   bucket = aws_s3_bucket.bucket.id
-  key    = "stop_ec2.zip"
+  key    = "server.zip"
   acl    = "private"
-  source = "${path.module}/stop_ec2.zip"
-  etag   = filemd5("${path.module}/stop_ec2.zip")
+  source = "${path.module}/server.zip"
+  etag   = filemd5("${path.module}/server.zip")
 }
 
 resource "aws_lambda_function" "lambda" {
   role          = aws_iam_role.role.arn
   s3_bucket     = aws_s3_bucket.bucket.id
-  s3_key        = "stop_ec2.zip"
+  s3_key        = "server.zip"
   function_name = "lambda-fn"
   handler       = "stop_instances.lambda_handler"
   runtime       = "python3.6"
@@ -138,6 +138,69 @@ EOF
 resource "aws_iam_role_policy_attachment" "iam-policy-attach" {
   role       = aws_iam_role.role.name
   policy_arn = aws_iam_policy.policy.arn
+}
+
+resource "aws_api_gateway_rest_api" "rest" {
+  name = "Serverless"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.rest.id
+  parent_id   = aws_api_gateway_rest_api.rest.root_resource_id
+  path_part   = "{proxy+}"
+}
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.rest.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id             = aws_api_gateway_rest_api.rest.id
+  resource_id             = aws_api_gateway_method.proxy.resource_id
+  http_method             = aws_api_gateway_method.proxy.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = aws_api_gateway_rest_api.rest.id
+  resource_id   = aws_api_gateway_rest_api.rest.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id             = aws_api_gateway_rest_api.rest.id
+  resource_id             = aws_api_gateway_method.proxy_root.resource_id
+  http_method             = aws_api_gateway_method.proxy_root.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "deploy_rest" {
+  depends_on = [
+    aws_api_gateway_integration.lambda,
+    aws_api_gateway_integration.lambda_root,
+  ]
+  rest_api_id = aws_api_gateway_rest_api.rest.id
+  stage_name  = "stop"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  # The "/*/*" portion grants access from any method on any resource
+  # within the API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.rest.execution_arn}/*/*"
+}
+
+output "base_url" {
+  value = aws_api_gateway_deployment.deploy_rest.invoke_url
 }
 
 output "server_id" {
