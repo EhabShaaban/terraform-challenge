3785d3fa0c205da7208a05f5ffe8ae8a70e62fcb
EhabShaaban <ehabsfeki@gmail.com>
2022-01-29 20:28:22 +0100

    Add tags endpoint

--- main.tf
+++ main.tf
@@ -143,27 +143,6 @@ resource "aws_api_gateway_rest_api" "rest" {
   name = "Serverless"
 }
 
-resource "aws_api_gateway_resource" "stop" {
-  rest_api_id = aws_api_gateway_rest_api.rest.id
-  parent_id   = aws_api_gateway_rest_api.rest.root_resource_id
-  path_part   = "stop"
-}
-resource "aws_api_gateway_method" "stop" {
-  rest_api_id   = aws_api_gateway_rest_api.rest.id
-  resource_id   = aws_api_gateway_resource.stop.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id             = aws_api_gateway_rest_api.rest.id
-  resource_id             = aws_api_gateway_method.stop.resource_id
-  http_method             = aws_api_gateway_method.stop.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.lambda.invoke_arn
-}
-
 resource "aws_api_gateway_method" "root" {
   rest_api_id   = aws_api_gateway_rest_api.rest.id
   resource_id   = aws_api_gateway_rest_api.rest.root_resource_id
@@ -179,14 +158,66 @@ resource "aws_api_gateway_integration" "root" {
   uri                     = aws_lambda_function.lambda.invoke_arn
 }
 
+resource "aws_api_gateway_resource" "stop" {
+  rest_api_id = aws_api_gateway_rest_api.rest.id
+  parent_id   = aws_api_gateway_rest_api.rest.root_resource_id
+  path_part   = "stop"
+}
+resource "aws_api_gateway_method" "stop" {
+  rest_api_id   = aws_api_gateway_rest_api.rest.id
+  resource_id   = aws_api_gateway_resource.stop.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "stop_lambda" {
+  rest_api_id             = aws_api_gateway_rest_api.rest.id
+  resource_id             = aws_api_gateway_method.stop.resource_id
+  http_method             = aws_api_gateway_method.stop.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+resource "aws_api_gateway_resource" "tags" {
+  rest_api_id = aws_api_gateway_rest_api.rest.id
+  parent_id   = aws_api_gateway_rest_api.rest.root_resource_id
+  path_part   = "tags"
+}
+resource "aws_api_gateway_method" "tags" {
+  rest_api_id   = aws_api_gateway_rest_api.rest.id
+  resource_id   = aws_api_gateway_resource.tags.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "tags_lambda" {
+  rest_api_id             = aws_api_gateway_rest_api.rest.id
+  resource_id             = aws_api_gateway_method.tags.resource_id
+  http_method             = aws_api_gateway_method.tags.http_method
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
 resource "aws_api_gateway_deployment" "deploy_stop" {
   depends_on = [
-    aws_api_gateway_integration.lambda,
+    aws_api_gateway_integration.stop_lambda,
     aws_api_gateway_integration.root,
   ]
   rest_api_id = aws_api_gateway_rest_api.rest.id
   stage_name  = "stop"
 }
+
+resource "aws_api_gateway_deployment" "deploy_tags" {
+  depends_on = [
+    aws_api_gateway_integration.tags_lambda,
+    aws_api_gateway_integration.root,
+  ]
+  rest_api_id = aws_api_gateway_rest_api.rest.id
+  stage_name  = "tags"
+}
+
 
 resource "aws_lambda_permission" "apigw" {
   statement_id  = "AllowAPIGatewayInvoke"
@@ -200,6 +231,10 @@ resource "aws_lambda_permission" "apigw" {
 
 output "stop" {
   value = aws_api_gateway_deployment.deploy_stop.invoke_url
+}
+
+output "tags" {
+  value = aws_api_gateway_deployment.deploy_tags.invoke_url
 }
 
 output "instance_id" {
