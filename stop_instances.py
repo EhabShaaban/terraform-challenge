6ac499ca175b0dcec54ff8139945dbd228bfe4b0
EhabShaaban <ehabsfeki@gmail.com>
2022-01-30 16:19:26 +0100

    Update server

--- stop_instances.py
+++ stop_instances.py
@@ -1,23 +1,34 @@
 import boto3
+import json
 
-# Boto Connection
 ec2 = boto3.resource('ec2', 'us-west-2')
 
 
 def lambda_handler(event, context):
-    # Filters
-    filters = [{
-        'Name': 'tag:Name',
-        'Values': ['AutoStop']
-    }
+
+    filters = [
+        {'Name': 'tag:Name', 'Values': ['auto stop']},
+        {'Name': 'instance-state-name', 'Values': ['running']}
     ]
 
-    # Filter running instances that should stop
-    instances = ec2.instances.filter(Filters=filters)
+    instance_collection = ec2.instances.filter(Filters=filters)
 
     # Retrieve instance IDs
-    instance_ids = [instance.id for instance in instances]
+    instance_id = [instance.id for instance in instance_collection]
 
-    # stopping instances
-    stopping_instances = ec2.instances.filter(
-        Filters=[{'Name': 'instance-id', 'Values': instance_ids}]).stop()
+    if(event['requestContext']['path'] == '/stop'):
+        ec2.instances.filter(
+            Filters=[{'Name': 'instance-id', 'Values': instance_id}]).stop()
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps("stopping "+str(instance_id)+"...")
+        }
+
+    elif(event['requestContext']['path'] == '/tags'):
+        instance_tags = [instance.tags for instance in instance_collection]
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(instance_tags, indent=4, sort_keys=True)
+        }
