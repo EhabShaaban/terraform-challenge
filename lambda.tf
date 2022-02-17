0448773f9ee1b7302ceaa9a37a15a11f0b24e280
EhabShaaban <ehabsfeki@gmail.com>
2022-02-17 20:08:45 +0100

    Update lambda policy

--- lambda.tf
+++ lambda.tf
@@ -12,6 +12,11 @@ resource "aws_lambda_function" "lambda" {
   ]
 }
 
+resource "aws_iam_role_policy_attachment" "iam-policy-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
 resource "aws_iam_policy" "policy" {
   name        = "lambda_access-policy"
   description = "IAM Policy"
@@ -25,21 +30,11 @@ resource "aws_iam_policy" "policy" {
                 "logs:CreateLogGroup",
                 "logs:CreateLogStream",
                 "logs:PutLogEvents",
-                "s3:ListAllMyBuckets",
-                "s3:GetBucketLocation"
+                "ec2:Stop*",
+                "ec2:DescribeInstances"
             ],
             "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-              "s3:*",
-              "ec2:Stop*",
-              "ec2:DescribeInstances",
-              "ec2:DescribeTags"
-            ],
-            "Resource": "*"
-        }
+    }
   ]
 }
   EOF
@@ -63,9 +58,4 @@ resource "aws_iam_role" "role" {
   ]
 }
 EOF
-}
-
-resource "aws_iam_role_policy_attachment" "iam-policy-attach" {
-  role       = aws_iam_role.role.name
-  policy_arn = aws_iam_policy.policy.arn
 }
