57b033845c8c4186c2e43befb02ba4ee5ace8784
EhabShaaban <ehabsfeki@gmail.com>
2022-02-25 09:00:53 +0100

    feat(modules): Add bucket module

---
+++ modules/bucket/main.tf
@@ -0,0 +1,12 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = aws_s3_bucket.bucket.id
+  key    = var.package_name
+  acl    = "private"
+  source = "${path.module}/${var.package_name}"
+  etag   = filemd5("./${var.package_name}")
+}
---
+++ modules/bucket/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  type        = string
+  description = "Bucket name"
+}
+
+variable "package_name" {
+  type        = string
+  description = "Package name"
+}
